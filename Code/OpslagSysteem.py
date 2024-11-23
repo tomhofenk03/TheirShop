60cebd72455ee9126d7ac49372e66aa35e6205ea
tomhofenk03 <tomhofenk@hotmail.com>
2024-11-23 15:20:45 +0100

    test van list en begin menu

---
+++ Code/OpslagSysteem.py
@@ -0,0 +1,6 @@
+#Dit bestand zal worden gebruikt als locale database, en zal worden aangeroepen in de functies van het programma
+
+voorraaditems = ['test', 'test2']
+
+
+print(voorraaditems)
---
+++ Code/StartMenu.py
@@ -0,0 +1,7 @@
+#Dit zal het begin zijn van het programma, en hier zal je de keuze krijgen voor welke functie je wilt gebruiken
+
+print("Welkom, en bedankt voor het kiezen voor TheirShop. Maak een keuze:")
+print("1: Geef voorraad weer.")
+print("2: Voeg een levering toe.")
+print("3: Voer een telling uit.")
+print("4: Voeg een item toe aan het systeem.")
